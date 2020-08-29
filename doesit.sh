131a27ea04b27bb75a4aa7cb4c04cc187e5a68a1
ilya /nrd /x1 <ialexs@narrada.com>
2020-08-29 14:59:15 +0700

    Define server to test not using our own ISP

--- doesit.sh
+++ doesit.sh
@@ -15,6 +15,13 @@
 # (using Zulu time format)
 date=$(date --utc +%FT%TZ)
 
+# To be fair. We don't test to our own ISP :)
+# Server list from `speedtest -L` in speedtest-list-from-XX.txt
+server_id=(12807 12909 13039 16919 11118 7582 12936 17288 7162)
+server_jp=(14623 20976 28910 15047 24333 32907 33558 34122 24537 8407)
+server_my=(1899 3758 5721 1701 11557 20140 10327 11850 8700 9454)
+server_sg=(23467 35031 13623 2054 367 4235 5935 7311 7556 26654)
+
 speedtest_log='speedtest_log.json'
 speedtest_result='speedtest_result'
 
@@ -22,11 +29,12 @@ speedtesting () {
 	# Empty previous test result
 	cat /dev/null > $speedtest_result
 
-	# - Manually define target server with `-s` (6612 = FirstMedia, Jakarta, Indonesia)
-	# - Change to `-f csv` if you want a simple CSV format.
-	#
+	# - Manually define target server with `-s` (6612 = FirstMedia, Jakarta, Indonesia. 797=Biznet)
 	# speedtest -s 6612 -f json > $speedtest_result
-	speedtest -f json > $speedtest_result
+	#
+	# - Change to `-f csv` if you want a simple CSV format.
+	# - shuffle from server_XX as you like
+	speedtest -s $(shuf -e ${server_id[@]} | head -n 1) -f json > $speedtest_result
 
 	if [ -s "$speedtest_result" ]
 	then
@@ -38,3 +46,4 @@ speedtesting () {
        }
 
 speedtesting
+
